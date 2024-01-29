da24329ecc4152c3b4ce9e463e25bdd6a768946e
adorahelen <adorahelenmin@gmail.com>
2024-01-29 22:20:41 +0900

    24_01_29 in Sogang Univ

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
-#include<stdio.h>
-// 진수변환 프로그램
-#define swap(type, x, y) do { type t = x; x = y; y = t; } while (0)
+// 다차원 배열
 
-int conv (unsigned x, int n, char d[])
+#include <stdio.h>
+
+int mdays[][12] = {{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+                   {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+// 각 달의 날 수
+
+int isYear(int year)
 {
-    char dchar[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int digits = 0;
+    return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+} //윤년 판별 공식
 
-    if (x == 0)
-        d[digits++] =
+int dayof_year(int y, int m, int d) {
+    int days = d; // 날 수
+    for (int i = 1; i < m; i++)
+        days += mdays[isYear(y)][i - 1]; // 이부분
+
+    return days;
+}// 그 해의 지난 날 수를 구하고 더한다.
+
+int main (void)
+{
+    int retry;
+    do {
+        int year
+    }
 }
+
