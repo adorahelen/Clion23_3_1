5c5322b653157ea46cd1307e3fc6c2174e4aa633
adorahelen <adorahelenmin@gmail.com>
2024-01-30 18:48:50 +0900

    24_01_30 in Central Liv

--- main.c
+++ main.c
@@ -1,29 +1,20 @@
-// 다차원 배열
+// stack
 
-#include <stdio.h>
+#ifndef ___InStack
+#define ___InStack
 
-int mdays[][12] = {{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-                   {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
-// 각 달의 날 수
+typedef struct {
 
-int isYear(int year)
-{
-    return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-} //윤년 판별 공식
+    int max; // 스택 용량
+    int ptr; // 스택 포인터
+    int *stk // 스택의 첫 요소에 대한 포인터
 
-int dayof_year(int y, int m, int d) {
-    int days = d; // 날 수
-    for (int i = 1; i < m; i++)
-        days += mdays[isYear(y)][i - 1]; // 이부분
+}; IntStack; // 스택이름
 
-    return days;
-}// 그 해의 지난 날 수를 구하고 더한다.
+int Initialize (IntStack *s, int max);
 
-int main (void)
-{
-    int retry;
-    do {
-        int year
-    }
-}
+int Push (IntStack *s, int x);
 
+int Pop(IntStack *s, int *x);
+
+int Peek
