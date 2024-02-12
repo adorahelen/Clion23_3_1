46d9e2a41a4cda8db51fa856bc7fe73f319084ca
adorahelen <adorahelenmin@gmail.com>
2024-02-12 16:10:14 +0900

    24_02_12 Sogang uni (Pilot2, Divide)

--- PilotProject/SASfunction.c
+++ PilotProject/SASfunction.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include "Header.h"
+#include "SASheader.h"
 
 void InserSort(STUDENT list[], int last) {
 
--- PilotProject/SASmain.c
+++ PilotProject/SASmain.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#include "Header.h"
+#include "SASheader.h"
 
 #define NUM_STU 5
 
