9ca1fc72ac849778d04b5a3c70600ce30e60fbd8
adorahelen <adorahelenmin@gmail.com>
2024-02-28 14:36:27 +0900

    24_02_28 Central lib

--- Real_MergeSorting.c
+++ Real_MergeSorting.c
@@ -22,11 +22,44 @@ static void __mergesort (int a[], int left, int right)
          * 재귀 호출을 사용하여 리스트를 계속해서 분할하고 정렬하는데.
          * 여기서는 '__mergesort' 함수가 자기 자신을 호출하여 리스트를 분할하고 정렬한다.
          * */
-        for (i = left; i <= center; i++)
+        for (i = left; i <= center; i++) // 정렬된 리스트를 병합하는 과정
             buff[p++] = a[i];
-        while (i <= right && j < p) // 이 부분 다시 공부
-            a[k++] = (buff[j] <= a[i] ? )
-
-                    // NOT
+        while (i <= right && j < p)
+            a[k++] = (buff[j] <= a[i]) ? buff[j++] : a[i++];
+        while(j < p)
+            a[k++] = buff[j++];
     }
 }
+
+int mergesort (int a[], int n) {
+    if ((buff = calloc(n, sizeof (int)))  == NULL)
+        return -1;
+    __mergesort(a, 0, n-1);
+
+    free(buff);
+    return 0;
+}
+
+int main (void) {
+
+    int nx;
+    puts(" Merge Sorting");
+    printf(" Numbers of elemental : ");
+    scanf("%d", &nx);
+    int *x = calloc(nx, sizeof(int));
+
+    for (int i = 0; i < nx; i++) {
+        printf(" x[%d]: ", i);
+        scanf("%d", &x[i]);
+
+    }
+
+    mergesort(x, nx);
+    puts("Sorted by Assending . ");
+    for (int i = 0; i < nx; i++)
+        printf("x[%d] = %d\n ", i, x[i]);
+    free(x);
+
+    return 0;
+}
+
